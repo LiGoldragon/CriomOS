273805746efb07a940e27a4b5ffeb901b5c7b299
li <li@goldragon.criome.net>
2026-07-11 09:43:54 +0200

    CriomOS: require remote judge pin-chain execution (gpt-5.6; source-guided)

--- checks/spirit-judge-pin-chain/default.nix
+++ checks/spirit-judge-pin-chain/default.nix
@@ -16,6 +16,9 @@ assert require (deploymentNodes.spirit.locked.rev == "f9f5266abec8a0bcf43b8bcc93
 assert require (deploymentNodes.spirit-judge.locked.rev == "c2303a30ff88fea527a8075b22f1d598a80fdb80") "unexpected spirit-judge revision";
 assert require (homeNodes.spirit-judge.locked.rev == "c2303a30ff88fea527a8075b22f1d598a80fdb80") "Home does not consume the witness producer";
 assert require (pkgs.lib.hasInfix "signal-spirit-judge.git?rev=7c25b71a34858c0d912dff8fd0b4f4ac213d7cd1" spiritCargoLock) "Spirit does not pin the approved signal contract";
-pkgs.runCommand "spirit-judge-pin-chain" { } ''
+pkgs.runCommand "spirit-judge-pin-chain" {
+  allowSubstitutes = false;
+  preferLocalBuild = false;
+} ''
   touch "$out"
 ''
