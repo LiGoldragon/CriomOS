9c6352fc54bb1b7bca89170655f7a147f1c16e70
li <li@goldragon.criome.net>
2026-07-26 01:42:26 +0200

    CriomOS: repin Home to final VSCodium lifecycle release

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     orchestrate.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home profile — its own repo, own inputs (niri, noctalia, stylix, emacs…).
-    criomos-home.url = "github:LiGoldragon/CriomOS-home/9db882f1baaaef0ef8d64e4a9e80de9872326071";
+    criomos-home.url = "github:LiGoldragon/CriomOS-home/5656a7692437496eb1dcb486146b6ad5b3cc545e";
     criomos-home.inputs.nixpkgs.follows = "nixpkgs";
     criomos-home.inputs.home-manager.follows = "home-manager";
     criomos-home.inputs.criomos-lib.follows = "criomos-lib";
