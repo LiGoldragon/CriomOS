df8236773c7e04635c889338f02d36f258403b1e
li <li@goldragon.criome.net>
2026-08-04 21:16:07 +0200

    CriomOS: use valid Nix socket mode bounds

--- modules/nixos/lojix.nix
+++ modules/nixos/lojix.nix
@@ -40,8 +40,8 @@ in
     };
 
     ordinarySocketMode = mkOption {
-      type = types.ints.between 0 0o777;
-      default = 0o660;
+      type = types.ints.between 0 511;
+      default = 432;
       description = "Unix mode encoded for the ordinary socket.";
     };
 
@@ -51,8 +51,8 @@ in
     };
 
     ownerSocketMode = mkOption {
-      type = types.ints.between 0 0o770;
-      default = 0o600;
+      type = types.ints.between 0 504;
+      default = 384;
       description = "Unix mode encoded for the owner/meta socket; other access is forbidden by Lojix.";
     };
 
