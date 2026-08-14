c8e8a543cad3bd09cb7c280632e2887b6b4fe4e8
li <li@goldragon.criome.net>
2026-08-14 04:20:33 +0200

    CriomOS: update CriomOS-home for niri+pi patch fixes

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     orchestrate.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home profile — its own repo, own inputs (niri, noctalia, stylix, emacs…).
-    criomos-home.url = "github:LiGoldragon/CriomOS-home/08cb17921c30885f2e803d7dd2e81de442607639";
+    criomos-home.url = "github:LiGoldragon/CriomOS-home/8d89c288dfacf187d8940d6c7006ed4fa7963bf1";
     criomos-home.inputs.nixpkgs.follows = "nixpkgs";
     criomos-home.inputs.home-manager.follows = "home-manager";
     criomos-home.inputs.criomos-lib.follows = "criomos-lib";
