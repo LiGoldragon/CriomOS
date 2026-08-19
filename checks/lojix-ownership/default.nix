e08e9d65af8c9234731afb5af9646ab463a1dc02
li <li@goldragon.criome.net>
2026-08-19 19:27:04 +0200

    Pin graphical agent TUI closure

--- checks/lojix-ownership/default.nix
+++ checks/lojix-ownership/default.nix
@@ -3,7 +3,7 @@ let
   lib = inputs.nixpkgs.lib;
   system = pkgs.stdenv.hostPlatform.system;
   expectedRevision = "7693ad3e8814cbea68ea6491d6a10f04d5cb2979";
-  expectedHomeRevision = "6bc9860df7d988ea0b63ed7c2dc6b5f0e6098c0c";
+  expectedHomeRevision = "51063e6a55ac94f64613f623c5c6601f8a965c99";
   expectedOrchestrateRevision = "45283e2120e930e62dabdaf650e704a425be804c";
   expectedSchemaRustRevision = "37b7d1035a472a15081f3e2e8a93b95bf733c3ee";
   rootLock = builtins.fromJSON (builtins.readFile ../../flake.lock);
